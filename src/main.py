bea27926cfbbd01351451c4aece310fb2efaeb7e
gsauzande <gastensauzande@hotmail.com>
2018-05-19 17:09:56 +0200

    Graph examples for data display

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import matplotlib.pyplot as plt
+#line chart
+x = [2, 4, 6]
+y = [1, 3, 5]
+plt.plot(x, y)
+plt.show()
+
+year = [1960, 1970, 1980, 1990, 2000, 2010]
+pop_pakistan = [44.91, 58.09, 78.07, 107.7, 138.5, 170.6]
+pop_india = [449.48, 553.57, 696.783, 870.133, 1000.4, 1309.1]
+plt.plot(year, pop_pakistan, color='g')
+plt.plot(year, pop_india, color='orange')
+plt.xlabel('Countries')
+plt.ylabel('Population in million')
+plt.title('Pakistan India Population till 2010')
+plt.show()
