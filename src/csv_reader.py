d09e9c5637fb717da793b8cd182f74cfdbb7325b
Mrunal Patil <m.patil@student.fontys.nl>
2018-05-21 22:07:26 +0200

    Function or csv file reader
    The function converts the data in csv files into arrays

--- src/csv_reader.py
+++ src/csv_reader.py
@@ -1,7 +1,9 @@
 import csv
+def myFunc(fileName):
+    with open(fileName, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            print(line)
+    return
 
-with open('Test Data - SOLAR - Building.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for line in csv_reader:
-        print(line)
+myFunc('Test Data - SOLAR - Building.csv')
