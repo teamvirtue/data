15d62291ff546e9f4704f2e67b914b03c887572e
gsauzande <gastensauzande@hotmail.com>
2018-06-01 13:39:02 +0200

    Updated the API endpoints

--- src/main.py
+++ src/main.py
@@ -9,13 +9,17 @@ api = Api(app)
 xls = pd.ExcelFile("../dataset/test_data_solar.xlsx")
 df1 = pd.read_excel(xls, 'Socket_Readings')
 print(df1.to_json(orient='table'))
-class Visualize(Resource):
-    def get(self):
-        resp = Response(response=df1.to_json(orient='table'),
-                        status=200,
-                        mimetype="application/json")
-        return (resp)
 
-api.add_resource(Visualize, '/visual') # Route_1
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+@app.route('/socket')
+def api_articles():
+    resp = Response(response=df1.to_json(orient='table'),
+                    status=200,
+                    mimetype="application/json")
+    return (resp)
+
 if __name__ == '__main__':
      app.run(port='5002')
